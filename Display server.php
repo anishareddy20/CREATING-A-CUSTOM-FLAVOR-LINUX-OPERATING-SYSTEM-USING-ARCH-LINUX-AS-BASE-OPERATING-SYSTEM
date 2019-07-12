c57fb4f4e19a87c5c6a4e51a0c335d0fa4c6f8c2
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:33:40 -0400

    Create Display server.php

---
+++ Display server.php
@@ -0,0 +1,9 @@
+<h2>Display server</h2>
+<label class="container">Xorg - Xorg is the most popular display server among Linux users. Its ubiquity has led to making it an ever-present requisite for GUI applications, resulting in massive adoption from most distributions. 
+		<input type="radio" name="displayserver" value="xorg-server" checked="checked">
+<span class="radio"></span>
+</label>
+<label class="container">Wayland - is a protocol for a compositor to talk to its clients, as well as a library implementing this protocol. Many major Linux desktop environments, like Gnome and KDE, support Wayland. 
+		<input type="radio" name="displayserver" value="weston">
+<span class="radio"></span>
+</label>
