6d3f16115b9002ecacff6902e1b7fa6307a145ea
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:39:30 -0400

    Create Vncclients.php

---
+++ Vncclients.php
@@ -0,0 +1,33 @@
+<h2>VNC Clients</h2>
+<label class="container">GNOME Boxes-  A simple GNOME 3 application to access remote or virtual systems. Supports VNC and SPICE
+    <input type="checkbox" name="vncclients[]" value=" gnome-boxes">
+    <span class="checkmark"></span>
+</label>
+<label class="container">GVncViewer -  Simple VNC Client on Gtk-VNC.
+    <input type="checkbox" name="vncclients[]" value="gtk-vnc">
+    <span class="checkmark"></span>
+</label>
+<label class="container">KRDC- Remote Desktop Client for KDE. Supports RDP and VNC. Part of kdenetwork.
+    <input type="checkbox" name="vncclients[]" value="krdc">
+    <span class="checkmark"></span>
+</label>
+<label class="container"> Remmina -  Remote desktop client written in GTK+. Supports RDP, VNC, NX, XDMCP and SSH.
+    <input type="checkbox" name="vncservers[]" value="remmina ">
+    <span class="checkmark"></span>
+</label>
+<label class="container">vncviewer (TigerVNC)- VNC viewer for X
+    <input type="checkbox" name="vncclients[]" value=" tigervnc">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Vinagre- Remote desktop viewer for GNOME. Supports RDP, VNC, SPICE and SSH. Part of gnome-extra.
+    <input type="checkbox" name="vncclients[]" value="vinagre">
+    <span class="checkmark"></span>
+</label>
+<label class="container">xfreerdp -  FreeRDP X11 client.
+    <input type="checkbox" name="vncclients[]" value="freerdp">
+    <span class="checkmark"></span>
+</label>
+<label class="container">X2Go Client- A graphical client (Qt4) for the X2Go system that uses the NX technology protocol.
+    <input type="checkbox" name="vncclients[]" value="x2goclient">
+    <span class="checkmark"></span>
+</label>
