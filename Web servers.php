f86e221f3093ea16246198b4bc29c79499fc64c3
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:40:56 -0400

    Create Web servers.php

---
+++ Web servers.php
@@ -0,0 +1,29 @@
+<h2>Web Servers</h2>
+<label class="container">Apache -  A high performance Unix-based HTTP server
+    <input type="checkbox" name="webservers[]" value="apache">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Hiawatha - Secure and advanced webserver
+    <input type="checkbox" name="webservers[]" value="hiawatha">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Lighttpd -   A secure, fast, compliant and very flexible web-server.
+    <input type="checkbox" name="webservers[]" value="lighttpd">
+    <span class="checkmark"></span>
+</label>
+<label class="container">nginx -   Lightweight HTTP server and IMAP/POP3 proxy server
+    <input type="checkbox" name="webservers[]" value="nginx">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Webfs -  Simple and instant http server for mostly static content.
+    <input type="checkbox" name="webservers[]" value="webfs ">
+    <span class="checkmark"></span>
+</label>
+<label class="container">darkhttpd-   A small and secure static webserver
+    <input type="checkbox" name="webservers[]" value="darkhttpd">
+    <span class="checkmark"></span>
+</label>
+<label class="container">yaws - Web server/framework written in Erlang
+    <input type="checkbox" name="webservers[]" value="yaws">
+    <span class="checkmark"></span>
+</label>
