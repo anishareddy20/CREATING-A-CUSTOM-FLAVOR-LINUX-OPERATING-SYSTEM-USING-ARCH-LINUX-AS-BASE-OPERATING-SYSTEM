e5684bc45af0253710d9f3aae89d66722790ce1b
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:37:09 -0400

    Create Network manger.php

---
+++ Network manger.php
@@ -0,0 +1,39 @@
+<h2>Network Managers</h2>
+<label class="container">ConnMan -  Daemon for managing internet connections within embedded devices running the Linux operating system. Comes with a command-line client, plus Enlightenment, ncurses, GTK and Dmenu clients are available
+    <input type="checkbox" name="networkmanagers[]" value="ConnMan">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">dhcpcd - RFC2131 compliant DHCP client daemon.
+    <input type="checkbox" name="networkmanagers[]" value="dhcpcd">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">Kea-  An open source implementation of the Dynamic Host Configuration Protocol (DHCP) servers.
+    <input type="checkbox" name="networkmanagers[]" value="kea">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">netctl- Simple and robust tool to manage network connections via profiles. Intended for use with systemd.
+    <input type="checkbox" name="networkmanagers[]" value="netctl">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">NetworkManager  - Manager that provides wired, wireless, mobile broadband and OpenVPN detection with configuration and automatic connection.
+    <input type="checkbox" name="networkmanagers[]" value="networkmanager">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">systemd-networkd- Native systemd daemon that manages network configuration. It includes support for basic network configuration through udev.
+    <input type="checkbox" name="networkmanagers[]" value="systemd">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">Wicd - Wireless and wired connection manager with few dependencies. Comes with an ncurses interface, and a GTK interface wicd-gtk is available.
+    <input type="checkbox" name="networkmanagers[]" value="wicd">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Wifi Radar - WiFi Radar is a Python/PyGTK2 utility for managing wireless (and only wireless) profiles. It enables you to scan for available networks and create profiles for your preferred networks
+    <input type="checkbox" name="networkmanagers[]" value="wifi-radar">
+    <span class="checkmark"></span>
+</label>
