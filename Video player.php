004a848dcd2d04e4a2d9bf85887114ace0637163
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:38:18 -0400

    Create Video player.php

---
+++ Video player.php
@@ -0,0 +1,12 @@
+<p class="p">Web browser</p>
+<label class="container">Google Chrome â€” Proprietary web browser developed by Google
+    <input type="checkbox" name="videoplayers[]" value="">
+    <span class="checkmark"></span>ok
+</label <label class="container">Google Chrome â€” Proprietary web browser developed by Google
+<input type="checkbox" name="videoplayers[]" value="">
+<span class="checkmark"></span>
+</label>
+<label class="container">Google Chrome â€” Proprietary web browser developed by Google
+    <input type="checkbox" name="videoplayers[]" value="">
+    <span class="checkmark"></span>
+</label>
