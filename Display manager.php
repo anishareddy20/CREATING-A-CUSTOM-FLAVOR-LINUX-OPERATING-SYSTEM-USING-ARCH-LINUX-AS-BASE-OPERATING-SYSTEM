9803040f18c73923c3826faaa2169457f748b6d4
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:33:10 -0400

    Create Display manager.php

---
+++ Display manager.php
@@ -0,0 +1,17 @@
+<h2>Display manager</h2>
+<label class="container">GDM
+		<input type="radio" name="displaymanager" value="gdm" checked="checked">
+<span class="radio"></span>
+</label>
+<label class="container">LightDM
+		<input type="radio" name="displaymanager" value="lightdm">
+<span class="radio"></span>
+</label>
+<label class="container">LXDM
+		<input type="radio" name="displaymanager" value="lxdm">
+<span class="radio"></span>
+</label>
+<label class="container">SDDM
+		<input type="radio" name="displaymanager" value="sddm">
+<span class="radio"></span>
+</label>
