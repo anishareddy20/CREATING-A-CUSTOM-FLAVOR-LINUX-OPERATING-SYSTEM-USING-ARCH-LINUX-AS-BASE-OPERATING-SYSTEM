3dca5c1c1187c7076c5c15796588889ba3a9af75
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:35:14 -0400

    Create Download manager.php

---
+++ Download manager.php
@@ -0,0 +1,19 @@
+<h2>Download manager</h2>
+<label class="container">Gwget - Download manager for GNOME. Supports HTTP and FTP.
+	<input type="checkbox" name="downloadmanagers[]" value="gwget">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">KGet - Download manager for KDE. Supports HTTP, FTP, BitTorrent and Metalink. Part of kdenetwork.
+    <input type="checkbox" name="downloadmanagers[]" value="kget">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Steadyflow - Simple download manager for GNOME. Supports HTTP and FTP.
+    <input type="checkbox" name="downloadmanagers[]" value="steadyflow">
+    <span class="checkmark"></span>
+</label>
+
+<label class="container">uGet - GTK+ download manager featuring download classification and HTML import. Supports HTTP, FTP, BitTorrent and Metalink.
+    <input type="checkbox" name="downloadmanagers[]" value="uget">
+    <span class="checkmark"></span>
+</label>
