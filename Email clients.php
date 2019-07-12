95cf4b40c9d41f7866b573e9d189042ec1e5984d
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:35:44 -0400

    Create Email clients.php

---
+++ Email clients.php
@@ -0,0 +1,53 @@
+<h2>Email clients</h2>
+<label class="container">Balsa - Simple and light email client for GNOME
+    <input type="checkbox" name="emailclients[]" value="balsa">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Claws Mail - Lightweight GTK-based email client and news reader
+    <input type="checkbox" name="emailclients[]" value="Claws Mail">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Evolution - Mature and feature-rich e-mail client that is part of the GNOME project. Part of gnome-extra.
+    <input type="checkbox" name="emailclients[]" value="Evolution">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Geary - Simple desktop mail client built in Vala.
+    <input type="checkbox" name="emailclients[]" value="Geary">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Kmail - Mature and feature-rich email client. Part of kdepim.
+    <input type="checkbox" name="emailclients[]" value="Kmail">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Mailpile - A modern, fast web-mail client with user-friendly encryption and privacy features.
+    <input type="checkbox" name="emailclients[]" value="Mailpile">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Mailspring - Fork of Nylas Mail
+    <input type="checkbox" name="emailclients[]" value="Mailspring">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Nextcloud Mail - An email app for NextCloud
+    <input type="checkbox" name="emailclients[]" value=" nextcloud-app-mail">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Roundcubemail - Browser-based multilingual IMAP client with a native application-like user interface.
+    <input type="checkbox" name="emailclients[]" value="Roundcubemail">
+    <span class="checkmark"></span>
+</label>
+<label class="container">SeaMonkey Mail and Newsgroups- Email client included in the SeaMonkey suite.
+    <input type="checkbox" name="emailclients[]" value=" seamonkey">
+    <span class="checkmark"></span>
+</label>
+<label class="container">sylpheed- Lightweight and user-friendly GTK+ email client.
+    <input type="checkbox" name="emailclients[]" value="sylpheed">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Thunderbird - Feature-rich email client from Mozilla written in GTK+.
+    <input type="checkbox" name="emailclients[]" value="Thunderbird">
+    <span class="checkmark"></span>
+</label>
+<label class="container">TrojitÃ¡- Qt IMAP email client. Only supports one IMAP account.
+    <input type="checkbox" name="emailclients[]" value="trojita">
+    <span class="checkmark"></span>
+</label>
