639c5d8d7082c00b8ea80af76d7e7e5ebd9f77c5
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:37:46 -0400

    Create Office.php

---
+++ Office.php
@@ -0,0 +1,50 @@
+<div id='title2'>
+<span>Bootloader:</span>
+ </div>
+ <label class="container">Abiword - is a word processor that provides a lighter alternative for LibreOffice Writer and OpenOffice Writer, while at the same time providing great functionality. Abiword supports many standard document types, such as ODF documents, Microsoft Word documents, WordPerfect documents, Rich Text Format documents and HTML web pages.
+    <input type="radio" name="office[]" value="abiword">
+    <span class="radio"></span>
+</label>
+<label class="container">Apache OpenOffice - It stores all your data in an international open standard format and can also read and write files from other common office software packages. It can be downloaded and used completely free of charge for any purpose.
+    <input type="radio" name="office[]" value="openofficeAUR ">
+    <span class="radio"></span>
+</label>
+<label class="container">Djvu- is a computer file format designed primarily to store scanned documents, especially those containing a combination of text, line drawings, indexed color images, and photographs.
+    <input type="radio" name="office[]" value="djvulibre">
+    <span class="radio"></span>
+</label>
+<label class="container">Docbook- 
+     <input type="radio" name="office[]" value="docbook-xml">
+    <span class="radio"></span>
+</label>
+<label class="container">Gnumeric-is a powerful spreadsheet application which can import and export in various formats including .csv, HTML, LaTeX, Lotus 1-2-3, OpenDocument Spreadsheet and Microsoft Excel. 
+    <input type="radio" name="office[]" value="gnumeric">
+    <span class="radio"></span>
+</label>
+<label class="container">Gobby- is a free collaborative editor supporting multiple documents in one session and a multi-user chat.
+    <input type="radio" name="office[]" value="gobby">
+    <span class="radio"></span>
+</label> 
+<label class="container">Ledger- is a powerful, double-entry accounting system that is accessed from the UNIX command-line
+    <input type="radio" name="office[]" value="Ledger">
+    <span class="radio"></span>
+</label>
+
+<label class="container">LibreOffice-  the free power-packed Open Source personal productivity suite for Windows, Macintosh and Linux, that gives you six feature-rich applications for all your document production and data processing needs: Writer, Calc, Impress, Draw, Math and Base.
+     <input type="radio" name="office[]" value="libreoffice-fresh">
+    <span class="radio"></span>
+</label>
+
+
+<label class="container">Lout- It reads a high-level description of a document similar in style to LaTeX and produces a PostScript.
+     <input type="radio" name="office[]" value="lout">
+    <span class="radio"></span>
+</label>
+<label class="container">MuPDF- is a lightweight PDF viewer and toolkit written in portable C
+     <input type="radio" name="office[]" value="mupdf">
+    <span class="radio"></span>
+</label>
+<label class="container">Zathura- is a highly customizable and functional document viewer. It provides a minimalistic and space saving interface as well as an easy usage that mainly focuses on keyboard interaction
+	<input type="radio" name="office[]" value="zathura">
+    <span class="radio"></span>
+</label>
