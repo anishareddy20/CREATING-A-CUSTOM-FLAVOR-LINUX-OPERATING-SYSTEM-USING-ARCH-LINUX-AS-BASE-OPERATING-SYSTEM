b89f49a2989f6463fdd1de0dfc51b5ee2a976c19
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:40:03 -0400

    Create Vncservers.php

---
+++ Vncservers.php
@@ -0,0 +1,47 @@
+<h2>VNC Servers</h2>
+<label class="container">Krfb -   VNC server for KDE. Part of kdenetwork
+    <input type="checkbox" name="vncservers[]" value="Krfb ">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Vino -  VNC server for GNOME. Part of gnome.
+    <input type="checkbox" name="vncservers[]" value="vino">
+    <span class="checkmark"></span>
+</label>
+<label class="container">x0vncserver (TigerVNC) -  VNC Server for X displays
+    <input type="checkbox" name="vncservers[]" value="tigervnc">
+    <span class="checkmark"></span>
+</label>
+<label class="container">x11vnc -  VNC server for real X displays.
+    <input type="checkbox" name="vncservers[]" value="x11vnc">
+    <span class="checkmark"></span>
+</label>
+<label class="container">X2Go Server  -  An open source remote desktop software that uses the NX technology protocol.
+    <input type="checkbox" name="vncservers[]" value="x2goserver">
+    <span class="checkmark"></span>
+</label>
+Vpnclients.php
+<h2>VPN Clients</h2>
+<label class="container">OpenConnect -Supports Cisco and Juniper VPNs.
+    <input type="checkbox" name="vpnclients[]" value="openconnect">
+    <span class="checkmark"></span>
+</label>
+<label class="container">OpenVPN - To connect to OpenVPN VPNs.
+    <input type="checkbox" name="vpnclients[]" value="openvpn">
+    <span class="checkmark"></span>
+</label>
+<label class="container">PPTP Client-To connect to PPTP VPNs, like Microsoft VPNs (MPPE). (insecure)
+    <input type="checkbox" name="vpnclients[]" value="pptpclient">
+    <span class="checkmark"></span>
+</label>
+<label class="container">strongSwan - IPsec-based VPN Solution
+    <input type="checkbox" name="vpnclients[]" value="strongswan">
+    <span class="checkmark"></span>
+</label>
+<label class="container">tinc - tinc is a free VPN daemon.
+    <input type="checkbox" name="vpnclients[]" value="tinc">
+    <span class="checkmark"></span>
+</label>
+<label class="container">Vpnc- To connect to Cisco 3000 VPN Concentrators.
+    <input type="checkbox" name="vpnclients[]" value="vpnc">
+    <span class="checkmark"></span>
+</label>
