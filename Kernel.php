c18350b30b08bd79b72a42bc8ca244576c6cd4b7
anishareddy20 <47364657+anishareddy20@users.noreply.github.com>
2019-07-11 22:36:41 -0400

    Create Kernel.php

---
+++ Kernel.php
@@ -0,0 +1,19 @@
+<h2>Linux kernel</h2>
+<label class="container">Stable - Vanilla Linux kernel and modules, with a few patches applied.
+
+
+		<input type="radio" name="kernel" value="linux" checked="checked">
+<span class="radio"></span>
+</label>
+<label class="container">Hardened - A security-focused Linux kernel applying a set of hardening patches to mitigate kernel and userspace exploits. It also enables more upstream kernel hardening features than linux along with user namespaces (with unprivileged usage disabled by default via a patch), audit and SELinux.
+		<input type="radio" name="kernel" value="linux-hardened">
+<span class="radio"></span>
+</label>
+<label class="container">Longterm - Long-term support (LTS) Linux kernel and modules.
+		<input type="radio" name="kernel" value="linux-lts">
+<span class="radio"></span>
+</label>
+<label class="container">ZEN kernel - Result of a collaborative effort of kernel hackers to provide the best Linux kernel possible for everyday systems.
+		<input type="radio" name="kernel" value="linux-zen">
+<span class="radio"></span>
+</label>
